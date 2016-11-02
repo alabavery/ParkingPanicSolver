abdb1c8c7fa952462fc5957c8a5bd69324d3e4c8
Al Avery <averyab0@sewanee.edu>
2016-11-01 19:36:23 -0500

    Cosmetic changes plus documentation

--- plotting.py
+++ plotting.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
 from matplotlib.collections import PatchCollection
 
 def plot_lot(arrangement):
@@ -13,41 +14,48 @@ def plot_lot(arrangement):
 						 ,[0,0], [1,0], [2,0], [3,0], [4,0], [5,0] ]
 
 
-	plt.close()
-	fig, ax = plt.subplots()
-	plt.axis([0, 6, 0, 6])
+	plt.close() # since we're going to be calling plot_lot() iteratively, close the last iteration before doing anything
+	fig, ax = plt.subplots() # create a new figure and axis
+	plt.axis([0, 6, 0, 6]) # set zoom/window of graph
+	ax.set_axis_bgcolor('0.6') # color parking lot
+	plt.gca().axes.xaxis.set_ticklabels([]) # remove xaxis labels
+	plt.gca().axes.yaxis.set_ticklabels([]) # remove yaxis labels
+
+
+	line = mlines.Line2D([6,6],[3,4],lw=15., alpha=0.8, color='yellow') # define a yellow line to indicate the exit
+	ax.add_line(line) # add line to graph
 
 	patches = []
 	for i,vehicle in enumerate(arrangement):
 
-		difference = vehicle[1] - vehicle[0]
+		difference = vehicle[1] - vehicle[0] # get the difference b/n first and second squares that vehicle occupies
 		if difference > 0:
-			if abs(difference) == 1:
-				height = 1
-				width = len(vehicle)
+			if abs(difference) == 1: # determine whether or not vehicle is horizontally oriented
+				height = 1 # it's horizontal, so its height is 1
+				width = len(vehicle) # it's horizontal, so its width is its length
 				location = board_switcher[vehicle[0]]
-			else:
-				height = len(vehicle)
-				width = 1
+			else: # vehicle must be vertically oriented
+				height = len(vehicle) # it's vertical, so its height is its length
+				width = 1 # it's vertical, so its width is 1
 				location = board_switcher[vehicle[-1]]
 		else:
-			if abs(difference) == 1:
+			if abs(difference) == 1: # determine whether or not vehicle is horizontally oriented
 				location = board_switcher[vehicle[-1]]
-				height = 1
-				width = len(vehicle)
-			else:
+				height = 1 # it's horizontal, so its height is 1
+				width = len(vehicle) # it's horizontal, so its width is its length
+			else: # vehicle must be vertically oriented
 				location = board_switcher[vehicle[0]]
-				height = len(vehicle)
-				width = 1
+				height = len(vehicle) # it's vertical, so its height is its length
+				width = 1 # it's vertical, so its width is 1
 
 		if i == 0:
-			patches.append(mpatches.Rectangle(location, width, height, color='r'))
+			patches.append(mpatches.Rectangle(location, width, height, color='r')) # if this vehicle is the hero, color red
 		else:
-			patches.append(mpatches.Rectangle(location, width, height))
+			patches.append(mpatches.Rectangle(location, width, height)) # otherwise just go with the default color
 		
 
 
 
 	collection = PatchCollection(patches, alpha=0.3, match_original=True)
 	ax.add_collection(collection)
-	plt.pause(0.7)
+	plt.pause(0.7) # pause for 0.7 seconds between each frame so that humans can see what's happening
